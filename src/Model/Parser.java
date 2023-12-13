3ab5ad0e19c2d7b283f24167bf5b9cf2863ca322
aleksey <alexeypanov29@gmail.com>
2023-12-13 09:17:45 +0300

    Add commit for Parser

--- src/Model/Parser.java
+++ src/Model/Parser.java
@@ -1,5 +1,7 @@
 package Model;
-
+/* Класс принимает полученные данные из RequestData и
+и рспарсивает их в массив строк
+ */
 public class Parser implements IParser{
     private String[] data;
     @Override
